7f12341419f03b368302add48a72b28abec8d2f3
Katerina-Papi <katerinajpapi1@gmail.com>
2024-07-17 12:58:34 +0100

    API retrieves data and normalised with timestamp

---
+++ coinmarketapi.py
@@ -0,0 +1,31 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+import pandas as pd
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'20',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '4ae4cf29-a671-4408-89cb-d7ca1a4d4835',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+df = pd.json_normalize(data['data'])
+df['timestamp'] = pd.to_datetime('now') # shows exactly when it was last run to check if automation has worked
+
+
+print(df)
