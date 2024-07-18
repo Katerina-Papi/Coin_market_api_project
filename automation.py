44df35e952f3440efa79da689f748df028bf92f7
Katerina-Papi <katerinajpapi1@gmail.com>
2024-07-18 17:06:13 +0100

    New df of aggregated data

--- automation.py
+++ automation.py
@@ -6,7 +6,7 @@ import os
 from time import time
 from time import sleep # amount of seconds it sleeps before running again
 
-df = pd.DataFrame()
+# df = pd.DataFrame()
 
 def run_api():
     global df # declares it as global variable
@@ -32,18 +32,28 @@ def run_api():
         print(e)
     
     df = pd.json_normalize(data['data']) 
-    df = df.map(lambda x: '{:.5f}'.format(x) if isinstance(x, (float)) else x)
     df['timestamp'] = pd.to_datetime('now') # shows exactly when it was last run to check if automation has worked
-    
+    df2 = df.groupby('name', sort=False)[[
+                'quote.USD.percent_change_1h', 
+                'quote.USD.percent_change_24h', 
+                'quote.USD.percent_change_7d', 
+                'quote.USD.percent_change_30d', 
+                'quote.USD.percent_change_60d', 
+                'quote.USD.percent_change_90d']].mean()
     
     if not os.path.isfile('coinmarketapi2.csv'):
         df.to_csv('coinmarketapi2.csv', header='column_names')
     else:
         df.to_csv('coinmarketapi2.csv', mode='a', header=False) # appending new data every minute to csv file
-           
+
+    if not os.path.isfile('coinmarketapi2_agg.csv'):
+        df2.to_csv('coinmarketapi2_agg.csv', header='column_names')
+    else:
+        df2.to_csv('coinmarketapi2_agg.csv', mode='a', header=False) # appending new data every minute to csv file       
 
 for i in range(3): # testing with 3 api retrievals over 3 minutes
     run_api()
     print('API ran successfully')
     sleep(60)
 exit() 
+
